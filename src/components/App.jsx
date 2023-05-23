63f70bc2456fd7b198633eecfd800f7301cae025
Tejesh Reddy <72600922+mety0r@users.noreply.github.com>
2023-05-24 04:19:10 +0530

    update to app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,25 @@ import React from "react";
 import Header from "./Header";
 import Note from "./Note";
 import Footer from "./Footer";
+import Notes from "../notes";
+
+
 
 function App() {
     return(
         <div>
          <Header/>
+
+        {Notes.map( (noteItem)  => {
+         return (
+            <Note 
+             title={noteItem.title}
+             content={noteItem.content}
+             />
+            );
+        })}
          <Footer/>
-         <Note/>
+         
          
         </div>
    
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ function Header() {
 
     return(
         <header>
-            <h1>Keeper</h1>
+            <h1>Stickwick</h1>
         </header>
         
     );
