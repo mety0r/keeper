a6665810f60bad6334269bdfbe24667bf2a7fab3
Tejesh Reddy <72600922+mety0r@users.noreply.github.com>
2023-05-24 01:47:49 +0530

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from "./Header";
+import Note from "./Note";
+import Footer from "./Footer";
+
+function App() {
+    return(
+        <div>
+         <Header/>
+         <Footer/>
+         <Note/>
+         
+        </div>
+   
+
+    );
+}
+
+export default App;
